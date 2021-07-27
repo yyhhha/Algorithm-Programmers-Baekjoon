da5683b5b1793760cbfb00c3c7b4da3377d51c4e
yyhhha <87046726+yyhhha@users.noreply.github.com>
2021-07-27 22:37:20 +0900

    Create Stock.java

---
+++ stock/Stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] prices) {
+    	
+        int len = prices.length;
+        int[] answer = new int[len];
+        int i, j;
+        for (i = 0; i < len; i++) {
+            for (j = i + 1; j < len; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
+                    break;
+            }
+        }
+        return answer;
+    }
+}
